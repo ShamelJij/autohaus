8f7af9c974083a2c46436f60f81516355e0ac539
shamel jij <shamel.jij@gmail.com>
2022-06-21 11:23:27 +0200

    client side search added

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 class Fahrzeug{
-    constructor(marke, modell, farbe, antrieb, preis){
+    constructor(marke, modell, farbe, antrieb, preis, headerFarbe){
         this.marke = marke;
         this.modell = modell;
         this.farbe = farbe;
         this.antrieb = antrieb;
         this.preis = preis;
+        this.headerFarbe = headerFarbe;
     }
     info(){
-        return "<div class='card p-2 m-2' style='width: 14rem;'><div class='card-header bg-warning'>" + this.marke + " " + this.modell + "</div><br><ul class='list-group list-group-flush'><li class='list-group-item'>" +
+        return "<div class='card p-2 m-2' style='width: 14rem;'><div class='card-header bg-" + this.headerFarbe + "'>" + this.marke + " " + this.modell + "</div><br><ul class='list-group list-group-flush'><li class='list-group-item'>" +
         "Farbe: " + this.farbe + "</li><li class='list-group-item'>" +
         "Antrieb: " + this.antrieb + "</li><li class='list-group-item'>" +
         "Preis: " + this.preis + " €</li><li class='list-group-item'>";
@@ -15,9 +16,10 @@ class Fahrzeug{
 }
 
 class PKW extends Fahrzeug{
-    constructor(marke, modell, farbe, antrieb, preis, sitze){
+    constructor(marke, modell, farbe, antrieb, preis, sitze, headerFarbe= "warning"){
         super(marke, modell, farbe, antrieb, preis);
         this.sitze = sitze;
+        this.headerFarbe = headerFarbe;
     }
     info(){
         return super.info() + "Sitzpläze: " + this.sitze + " </li></div>";
@@ -25,9 +27,10 @@ class PKW extends Fahrzeug{
 }
 
 class LKW extends Fahrzeug{
-    constructor(marke, modell, farbe, antrieb, preis, ladung) {
+    constructor(marke, modell, farbe, antrieb, preis, ladung, headerFarbe = "secondary") {
         super(marke, modell, farbe, antrieb, preis, ladung);
         this.ladung = ladung;
+        this.headerFarbe = headerFarbe;
     }
     info(){
         return super.info() + "Ladung: " + this.ladung + " kg</li></div>";
@@ -38,12 +41,33 @@ let fahrzeug = [];
 fahrzeug.push(new PKW("Audi", "A8", "Anthrazit", "Allrad", 12000, 4));
 fahrzeug.push(new PKW("BMW", "M3", "Weiß", "Heckantrieb", 85000, 5));
 fahrzeug.push(new PKW("Ford", "Focus", "Schwarz", "Frontantrieb", 35000, 5));
-fahrzeug.push(new LKW("LADA", "Niva", "Rost", "Allrad", 18000, 57));
+fahrzeug.push(new PKW("LADA", "Niva", "Rost", "Allrad", 18000, 57));
+fahrzeug.push(new LKW("Volvo", "FH", "Grün", "Frontantrieb", 650000, 85));
+fahrzeug.push(new LKW("Volvo", "FH", "Grün", "Frontantrieb", 650000, 85));
 fahrzeug.push(new LKW("Volvo", "FH", "Grün", "Frontantrieb", 650000, 85));
 
 fahrzeug.sort(function(a,b){return a.preis-b.preis});
 
 for(let i = 0; i < fahrzeug.length; i++){
-    document.getElementById("info").innerHTML += fahrzeug[i].info();
+    if(fahrzeug[i].constructor.name == "PKW"){
+        document.getElementById("infoLinks").innerHTML += fahrzeug[i].info();
+    } else if (fahrzeug[i].constructor.name == "LKW") {
+        document.getElementById("infoRechts").innerHTML += fahrzeug[i].info();
+    }
 }
+    function suche(){
+        document.getElementById("infoLinks").innerHTML = ""; //Seite leer
+        document.getElementById("infoRechts").innerHTML = ""; //Seite leer
+        var such = document.getElementById("suche").value.toUpperCase();
+        for(let i = 0; i < fahrzeug.length; i++){
+            if(fahrzeug[i].marke.toUpperCase().includes(such)){
+                if(fahrzeug[i].constructor.name == "PKW"){
+                    document.getElementById("infoLinks").innerHTML += fahrzeug[i].info();
+                } else if (fahrzeug[i].constructor.name == "LKW") {
+                    document.getElementById("infoRechts").innerHTML += fahrzeug[i].info();
+                }
+            }
+        }
+}
+
 
