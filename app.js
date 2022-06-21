150d06068fa0a06a382c8337357759b97096f71a
shamel jij <shamel.jij@gmail.com>
2022-06-21 09:26:54 +0200

    added app.js

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ class Fahrzeug{
         this.antrieb = antrieb;
     }
     info(){
-        return "<b>" + this.marke + " " + this.modell + "</b><br>" +
-        "Farbe: " + this.farbe + "<br>" +
-        "Antrieb: " + this.antrieb;
+        return "<div class='card p-2 m-2' style='width: 12rem;'><div class='card-header bg-warning'>" + this.marke + " " + this.modell + "</div><br><ul class='list-group list-group-flush'><li class='list-group-item'>" +
+        "Farbe: " + this.farbe + "</li><li class='list-group-item'>" +
+        "Antrieb: " + this.antrieb + "</li><br>";
     }
 }
 
+
+let fahrzeug = [];
 let fahrzeug1 = new Fahrzeug("Audi", "A8", "Anthrazit", "Allrad");
 let fahrzeug2 = new Fahrzeug("BMW", "M3", "Weiß", "Heckantrieb");
 
+fahrzeug.push(fahrzeug1);
+fahrzeug.push(fahrzeug2);
+
 document.getElementById("info").innerHTML= fahrzeug1.info();
+document.getElementById("info").innerHTML += fahrzeug2.info();  
+document.getElementById("info").innerHTML += fahrzeug2.info();  
+document.getElementById("info").innerHTML += fahrzeug2.info();  
+document.getElementById("info").innerHTML += fahrzeug2.info();  
+document.getElementById("info").innerHTML += fahrzeug2.info();  
+document.getElementById("info").innerHTML += fahrzeug2.info();  
+document.getElementById("info").innerHTML += fahrzeug2.info();  
 document.getElementById("info").innerHTML += fahrzeug2.info();  
